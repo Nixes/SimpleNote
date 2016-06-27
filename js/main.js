e789b945228e50fc42a73501976cad49b0414feb
Nixes <callum.l.bryant@gmail.com>
2016-06-27 16:51:06 +1000

    formatting improvements + comment clean up

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-var pixelBufferFetch= 200 ; // number of pixels to leave between end of last page of notes and the next to fetch, depends on time required to get next page and user scroll velocity
-
+// number of pixels to leave between end of last page of notes and the next to fetch, depends on time required to get next page and user scroll velocity
+var pixelBufferFetch= 200 ;
 var notes = {
   notesPerRequest: 15,
   currentPage: 0,
   status: 0,
   note: [],
   getXmlhttp: function () {
-      // boilerplate compatibility code from w3schools
-      //var xmlhttp;
-      if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-          return new XMLHttpRequest();
+      if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        return new XMLHttpRequest();
       }
-      else {// code for IE6, IE5
-          return new ActiveXObject("Microsoft.XMLHTTP");
+      else {
+        // code for IE6, IE5
+        return new ActiveXObject("Microsoft.XMLHTTP");
       }
   },
   remove: function (nodeToRemove) {
@@ -32,43 +32,47 @@ var notes = {
                                 notes.note.splice(i, 1);
                             }
                         }
-                        nodeToRemove.remove(); // this has come compatability issues with older browsers
+                        // this has some compatability issues with older browsers
+                        nodeToRemove.remove();
                     }
                 }
             };
             xmlhttp.open("POST", "noteQuery.php", true);
-            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); // provides information in header that it is a form
+
+            // provides information in header telling sever that this request is from a form
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             xmlhttp.send("notesRequest_Type=remove&noteNo=" + nodeToRemoveId);
         }
   },
-  parser: function (noteContent) { // linkify's note content
-      // http://, https://, ftp://
-        var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+  parser: function (noteContent) {
+    // linkify note content
+    // http://, https://, ftp://
+    var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
 
-        // www. sans http:// or https://
-        var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    // www. sans http:// or https://
+    var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
 
-        // Email addresses
-        var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
-    // wow this looks neat, from https://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
-        tmp = noteContent
-            .replace(urlPattern, '<a href="$&">$&</a>')
-            .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
-            .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
-        tmp = tmp.replace(/[\n\r]/g, '<br />'); // converts newlines to standard html line breaks
+    // Email addresses
+    var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+    // from https://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
+    tmp = noteContent
+        .replace(urlPattern, '<a href="$&">$&</a>')
+        .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+        .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+    tmp = tmp.replace(/[\n\r]/g, '<br />'); // converts newlines to standard html line breaks
 
-        return tmp;
+    return tmp;
   },
   display: function (pageNotes) {
     for (i=0;i< pageNotes.length; i++) {
       var newNote = document.createElement('div');
       newNote.setAttribute('class','note');
-      newNote.setAttribute('id',pageNotes[i][1]); // set id to note no
+      newNote.setAttribute('id',pageNotes[i][1]); // set id to note number
       pageNotes[i][0] = this.parser(pageNotes[i][0] );
       newNote.innerHTML = '<p>' + pageNotes[i][0] + '</p><div class=edit>edit</div><div class=delete>del</div>';
       //newNote.style.cursor = 'pointer';
       //newNote.onclick = function (e) {
-      //    console.log("The origin element was " + this.getAttribute('id')); // note that this. does not work correctly ie < 8
+      //    console.log("The origin element was " + this.getAttribute('id')); // note that this does not work correctly ie < 8
       //    //this.style.backgroundColor = 'rgb(109, 143, 187)';
       //  var r = confirm("Are you sure you want to delete?");
       //  if (r==true) {
@@ -76,7 +80,7 @@ var notes = {
       //  }
         //};
       newNote.onmouseover = function (e) {
-          //console.log("The origin element was " + this.getAttribute('id')); // note that this. does not work correctly ie < 8
+          //console.log("The origin element was " + this.getAttribute('id')); // note that this does not work correctly ie < 8
           for (var i = 0; i < this.childNodes.length; i++) {
               if (this.childNodes[i].className == "edit") {
                   this.childNodes[i].style.display = "block";
@@ -93,7 +97,7 @@ var notes = {
           }
       };
       newNote.onmouseout = function (e) {
-          //console.log("The origin element was " + this.getAttribute('id')); // note that this. does not work correctly ie < 8
+          //console.log("The origin element was " + this.getAttribute('id')); // note that this does not work correctly ie < 8
           for (var i = 0; i < this.childNodes.length; i++) {
               if ( (this.childNodes[i].className == "edit") || (this.childNodes[i].className == "delete") ) {
                   this.childNodes[i].style.display = "none";
@@ -103,8 +107,9 @@ var notes = {
       document.getElementById('notes').appendChild(newNote);
     }
   },
-  // function to check for updates, check to see if there are any notes with a higher ID number than we ciurreently have
-  // if there are, they will be sent back and you can just display them
+
+  // function to check for updates, check to see if there are any new notes with a higher ID number than we currently have
+  // if there are, they will be returned like a normal page request
   update: function () {
       var xmlhttp = notes.getXmlhttp();
       xmlhttp.onreadystatechange=function() {
@@ -124,7 +129,7 @@ var notes = {
       note = document.getElementById("noteContent").value;
       var xmlhttp = notes.getXmlhttp();
     xmlhttp.open("POST","noteQuery.php",true);
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded"); // provides information in header that it is a form
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlhttp.send("notesRequest_Type=add&note=" + note);
   },
 
@@ -137,9 +142,9 @@ var notes = {
               pageNotes = JSON.parse(xmlhttp.responseText);
               if (pageNotes.length > 0) { // check to see if empty
                   console.log("notes.length was: " + pageNotes.length);
-                  notes.note = notes.note.concat(pageNotes); // fingers crossed here TODO: uncross fingers, this may have performance issues down the track though
-                  notes.display(pageNotes); // this is really shitty but seemingly unavoidable as it is an event that is executed externally
-                  notes.currentPage++; // increment current page
+                  notes.note = notes.note.concat(pageNotes);
+                  notes.display(pageNotes);
+                  notes.currentPage++;
               }
               notes.changePageStatus(0);
           }
@@ -151,8 +156,8 @@ var notes = {
     xmlhttp.send("notesRequest_Type=page&notesRequest_LastGroupNo="+this.notesPerRequest+"&noteRequest_Page=" + this.currentPage);
   },
   changePageStatus: function (setStatus) {
-  // a bunch of status's defined by a number here
-  var statusString = "";
+    // a bunch of status's defined by a number here
+    var statusString = "";
     switch (setStatus) {
       case 0:
         statusString = "idle";
@@ -167,13 +172,12 @@ var notes = {
   }
 };
 
-function checkScroll () { // only his noodliness knows the compatibility of this thing, mobile browsers seem not to like this, need alternatives...
-  // some shitty compatibility stuff here
-  var body = document.body,
-    html = document.documentElement;
+// this function has historically had issues with mobile browsers
+function checkScroll () {
+  // compatibility boilerplate
+  var body = document.body,html = document.documentElement;
   var documentHeight = Math.max( body.scrollHeight, body.offsetHeight,
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
-  // end shitty compatibility
 
   console.log( "window.pageYOffset: " + window.pageYOffset + " documentHeight " + documentHeight + " window.innerHeight: " + window.innerHeight ); // debugness
   if ( window.pageYOffset + window.innerHeight > (documentHeight - pixelBufferFetch)  & notes.status === 0) { // was if ( window.pageYOffset + window.innerHeight > (documentHeight - 50)  & notes.status == 0) {
@@ -181,29 +185,28 @@ function checkScroll () { // only his noodliness knows the compatibility of this
   }
 }
 
-// this function will use get page until the current page is full
+// this function calls getPage until the current page is full
 function fillPage () {
-    // some shitty compatibility stuff here
+    // compatability boilerplate
     var body = document.body;
     var html = document.documentElement;
     var documentHeight = Math.max( body.scrollHeight, body.offsetHeight,
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
-    // end shitty compatibility
 
-    if ((window.innerHeight + pixelBufferFetch) > documentHeight) { // was if ((window.innerHeight + 50) > documentHeight) {
-        if (notes.status === 0) { // simple blocking system, prevents notes from loading in wrong order TODO: FIX ,this may have issues when scrolling too fast, may need some kind of queuing system
-            console.log("A page was requested during filling");
-            notes.getPage();
-        }
+    if ((window.innerHeight + pixelBufferFetch) > documentHeight) {
+      // simple blocking system, prevents notes from loading in wrong order TODO: FIX. This may have issues when scrolling too fast, we may need some kind of queuing system
+      if (notes.status === 0) {
+        console.log("A page was requested during filling");
+        notes.getPage();
+      }
     }
 }
 
 
 function init () {
-   // window.setInterval(fillPage, 1000); // todo: auto destruct this event after page filled
-    fillPage();
+  fillPage();
 
-  window.onscroll = checkScroll; // only check after the page has loaded
+  window.onscroll = checkScroll;
   // a touchmove event on mobile devices may be a good alternative
 }
 window.onload = init;
