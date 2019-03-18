b849285ebfbca37a7fa86570b7b4cac23be2571e
Callum Bryant <callum.l.bryant@gmail.com>
2019-03-18 23:40:27 +1100

    simplify index

--- src/index.php
+++ src/index.php
@@ -27,26 +27,5 @@
   </div>
 </div>
 <div id="dim" onclick="hideDim()"></div>
-<?php
-include __DIR__.'../../user_validate.php';
-
-// load settings file
-require_once (__DIR__.'../../settings.php');
-    $conn = @mysqli_connect($host,
-    $user,
-    $pwd,
-    $db
-) or die("<p>The application failed to connect to the database server</p>");
-
-// check if notes table exists, if not create it
-$result = mysqli_query($conn, "SELECT * FROM notes;");
-if ($result == FALSE) {
-    echo "<p>Table does not exist, it is being created.</p>";
-    mysqli_query($conn, "CREATE TABLE notes (
-                                        note Varchar(10000),
-                                        noteNo Int unique
-                                        ); " ); // use of int to store note number has downside of only allowing maximum of 2147483647 possible notes, must be unique to prevent collisions
-}
-?>
 </body>
 </html>
