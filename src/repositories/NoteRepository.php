96e19593187e66246050f5c5c1e3b8318a0026d8
Callum Bryant <callum.l.bryant@gmail.com>
2019-03-18 23:40:16 +1100

    convert more data retrieval to repos

--- src/repositories/NoteRepository.php
+++ src/repositories/NoteRepository.php
@@ -2,41 +2,122 @@
 namespace SimpleNote\Repositories;
 
 use SimpleNote\Models\Note;
+use PDOException;
+use Exception;
 
-include __DIR__.'../../user_validate.php';
 
 class NoteRepository {
-    private $conn;
-    public function construct(\PDO $conn) {
-        $this->conn = $conn;
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
+    public function __construct(\PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @return bool
+     * @throws Exception
+     */
+    public function checkTableExists() {
+        $query = "SELECT * FROM notes;";
+        try {
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+
+            if ($stmt->errorCode() != '0000') {
+                throw new Exception($stmt->errorInfo()[2]);
+            }
+
+            if (!($row = $stmt->fetch(PDO::FETCH_ASSOC))) {
+                return false;
+            }
+
+            return true;
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function createTable() {
+        $query = "CREATE TABLE notes (note Varchar(10000),noteNo Int unique); ";
+        try {
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
+
+            if ($stmt->errorCode() != '0000') {
+                throw new Exception($stmt->errorInfo()[2]);
+            }
+
+            if (!($row = $stmt->fetch(PDO::FETCH_ASSOC))) {
+                return false;
+            }
+
+            return true;
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * @return bool
+     * @throws Exception
+     */
+    public function createTableIfNotExist() {
+        $createdTable = false;
+        // check if notes table exists, if not create it
+        if ($this->checkTableExists() == false) {
+            $this->createTable();
+            $createdTable = true;
+        }
+        return $createdTable;
     }
 
     public function insertNote (Note $note) {
+        try {
+            $sql = "INSERT INTO notes (note) VALUES(:content); ";
 
-            $note = mysqli_escape_string($conn,  filterNoteContents($_POST["note"]) );
-            $noteNoQuery = mysqli_query($conn, "SELECT MAX(noteNo) FROM notes;"); // note number is maximum note number plus one
-            $noteNoResult = mysqli_fetch_row($noteNoQuery);
-            $noteNo =  $noteNoResult[0] + 1;
-            //echo "<p> {$noteNo} </p>" ;
-            $insertQuery = "INSERT INTO notes VALUES(
-                                    '$note',
-                                    '$noteNo'
-                                    ); ";
-            $insertData = mysqli_query($conn, $insertQuery);
+            $stmt = $this->dbh->prepare($sql);
+            $stmt->execute(array(
+                ":noteNo" => $note->getId(),
+                ":note" => $note->getContent(),
+            ));
+
+            if ($stmt->errorCode() != '0000') {
+                throw new Exception($stmt->errorInfo()[2]);
+            }
+
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 
+    /**
+     * @param array $noteIds
+     * @return Note[]
+     * @throws Exception
+     */
+    public function findForIds(array $noteIds) {
+        $results = array();
+        $noteIdsString = implode(',',$noteIds);
+        $query = "SELECT * FROM `notes` WHERE noteNo in (".$noteIdsString.")";
+        try {
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute();
 
-    public function getNotesElementNo($conn,$Request_Elements) {
-        $ResultNotes = array();
-        for ($i=0; $i < sizeof($Request_Elements); $i++) {
-            if ( $noteQuery = mysqli_query($conn, "SELECT * FROM `notes` WHERE noteNo=$Request_Elements[$i]") ) {
-                while ($note = mysqli_fetch_row($noteQuery) ) {
-                    //echo '$note[0]','$note[1]' ;
-                    $ResultNotes[$i] = $note;
-                };
-            };
+            if ($stmt->errorCode() != '0000') {
+                throw new Exception($stmt->errorInfo()[2]);
+            }
+
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $results[] = self::build($row);
+            }
+
+            return $results;
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
         }
-        echo $ResultNotes;
     }
 
     public function getNotesPage($conn,$LastGroupNo, $Page) {
@@ -89,6 +170,17 @@ class NoteRepository {
         } else {
             echo "failed - validation";
         }
+    }
+
+    /**
+     * @param array $row
+     * @return Note
+     */
+    static public function build(array $row) {
+        $note = new Note();
+        $note->setContent($row['note']);
+        $note->setId($row['noteNo']);
+        return $note;
     }
 }
 ?>
