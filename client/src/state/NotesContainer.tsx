48a1b9f6254bd9d6de743fe89b9cc7905a4c483d
Callum Bryant <callum.l.bryant@gmail.com>
2019-05-05 14:46:36 +1000

    add code to retrieve new pages

--- client/src/state/NotesContainer.tsx
+++ client/src/state/NotesContainer.tsx
@@ -12,6 +12,49 @@ interface NotesState {
 	notes: Map<number,Note>;
 }
 
+type APINote = { id: string, content: string };
+
+class NoteAPI {
+	static async addNote(noteContent: string):Promise<APINote> {
+		const options = {
+			mode: "cors",
+			method:"PUT",
+			body:JSON.stringify({
+				content: noteContent}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+		// @ts-ignore
+		const result = await fetch(API_ROOT + '/notes', options);
+		const addedNote: APINote = await result.json();
+		return addedNote;
+	}
+
+	static async removeNote(noteId: number):Promise<void> {
+		const options = {
+			mode: "cors",
+			method:"DELETE"
+		};
+		// @ts-ignore
+		const result = await fetch(API_ROOT + '/notes/'+noteId, options);
+		const response = await result.json();
+		return;
+	}
+
+	static async getPage(pageNumber: number):Promise<APINote[]> {
+		const pageSize = 20;
+		const options = {
+			mode: "cors",
+			method:"GET"
+		};
+		// @ts-ignore
+		const result = await fetch(API_ROOT + `/notes?pageSize=${pageSize}&page=${pageNumber}`, options);
+		const response = await result.json();
+		return response;
+	}
+}
+
 export class NotesContainer extends Container<NotesState> {
 	constructor() {
 		super();
@@ -22,54 +65,44 @@ export class NotesContainer extends Container<NotesState> {
 		};
 	}
 
-	public getPage = () => {
+	public getNewPage = async () => {
+		const nextPage = this.state.currentPage + 1;
+		const apiNotes = await NoteAPI.getPage(nextPage);
+		const notes = apiNotes.map((apiNote)=>{
+			return {id: parseInt(apiNote.id), content: apiNote.content};
+		});
+		this.stateGetPage(notes);
 
-	}
+		// update page number
+		const state = this.state;
+		state.currentPage = nextPage;
+		this.setState(state);
+	};
 
 	public addNote = async (noteContent) => {
-			// const apiUrl = "noteQuery.php";
-			// const body = "notesRequest_Type=add&note=" + noteContent;
-			//
-			// // replace old code with new fetch syntax
-			// const response = await fetch(apiUrl,{
-			// 	method:"POST",
-			// 	headers: [["Content-type","application/x-www-form-urlencoded"]],
-			// 	body:body
-			// });
-			// console.log("Add note API result: ");
-			// console.log(response.json());
-		const options = {
-				mode: "cors",
-				method:"PUT",
-				body:JSON.stringify({
-					content: noteContent}),
-				headers: {
-					"Content-Type": "application/json"
-				}
-		};
 		try {
-			// @ts-ignore
-			const result = await fetch(API_ROOT+'/notes',options);
-			console.log("Addnote response: ");
-			console.log(result);
+			const addedNote = await NoteAPI.addNote(noteContent);
+			const noteId = parseInt(addedNote.id);
+
+			const note:Note = {
+				id:noteId,
+				content:noteContent
+			};
+			const previousState = this.state;
+			this.setState(previousState);
+			this.stateAddNote(note);
 		} catch (e) {
 			console.error(e);
 		}
-		console.log(options);
-
-		let nextId = this.state.lastNoteId+1;
-		const note:Note = {
-			id:nextId,
-			content:noteContent
-		};
-		const previousState = this.state;
-		previousState.lastNoteId = nextId;
-		this.setState(previousState);
-		this.stateAddNote(note);
 	}
 
 	public removeNote = async (id:number) => {
-		this.stateRemoveNote(id);
+		try {
+			NoteAPI.removeNote(id);
+			this.stateRemoveNote(id);
+		}catch (e) {
+			console.error(e);
+		}
 	}
 
 	public stateAddNotes = (newNotes:Note[]) => {
@@ -80,6 +113,10 @@ export class NotesContainer extends Container<NotesState> {
 		}
 
 		this.setState({ notes: notes});
+	}
+
+	private stateGetPage = (pageNotes: Note[]) => {
+		this.stateAddNotes(pageNotes);
 	}
 
 	private stateAddNote = (newNote:Note) => {
